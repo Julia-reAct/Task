32d411c7f233bba1870a318dc6e129e380a5806d
Yulia <yuliakiss369@gmail.com>
2024-06-20 20:43:56 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import AppBuffer from './AppBuffer'
-import AppBufferet from './AppBufferet'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
